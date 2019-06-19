2967778206a876edfcdc1171f12ac2d1cb50c5e6
Eric <ericsohny@gmail.com>
2019-06-19 17:44:27 -0400

    Add models and relationships

--- models.js
+++ models.js
@@ -17,17 +17,85 @@ const User = sequelize.define('users', {
   password_digest: {
     type: Sequelize.STRING,
     allowNull: false
+  },
+  username: {
+    type: Sequelize.STRING,
+    allowNull: false
+  }, 
+  image: {
+    type: Sequelize.STRING,
+    allowNull: true
   }
 });
 
-User.hasMany(Dragon, {
+const Message = sequelize.define('messages', {
+  text: {
+    type: Sequelize.STRING,
+    allowNull: false
+  }
+});
+
+const Chatroom = sequelize.define('chatrooms', {
+  genre: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  title: {
+    type: Sequelize.STRING,
+    allowNull: false
+  }, 
+  image: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  end_time: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  num_people: {
+    type: Sequelize.INTEGER,
+    allowNull: false 
+  },
+  limit_people: {
+    type: Sequelize.INTEGER,
+    allowNull: true
+  }
+});
+
+User.hasMany(Chatrooms, {
   onDelete: 'cascade',
   foreignKey: {
     allowNull: false
   }
 });
 
-Dragon.belongsTo(User, {
+Chatroom.belongsTo(User, {
+  foreignKey: {
+    allowNull: false
+  }
+});
+
+User.hasMany(Messages, {
+  onDelete: 'cascade',
+  foreignKey: {
+    allowNull: false
+  }
+});
+
+Message.belongsTo(User, {
+  foreignKey: {
+    allowNull: false
+  }
+});
+
+Chatroom.hasMany(Messages, {
+  onDelete: 'cascade',
+  foreignKey: {
+    allowNull: false
+  }
+});
+
+Message.belongsTo(Chatroom, {
   foreignKey: {
     allowNull: false
   }
@@ -35,5 +103,7 @@ Dragon.belongsTo(User, {
 
 module.exports = {
   sequelize,
-  User                                                                
+  User,                                                             
+  Message,
+  Chatroom
 };
