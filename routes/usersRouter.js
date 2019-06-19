d80e2ff3ccfdf590e88b69c0a45384cbe9ad58b8
Eric <ericsohny@gmail.com>
2019-06-19 17:28:56 -0400

    Add usersRouter

---
+++ routes/usersRouter.js
@@ -0,0 +1,120 @@
+const express = require('express');
+const { User } = require('../models');
+const { hash, compare, encode, verify, restrict } = require('../auth');
+
+const usersRouter = express.Router();
+
+//Get all users
+usersRouter.get('/', async (req, res) => {
+  try {
+    const users = await User.findAll();
+    res.json({ users });
+  } catch (e) {
+    console.log(e);
+    res.stats(500).send(e.message);
+  }
+});
+
+//Get a specific user
+usersRouter.get('/:id', async (req, res) => {
+  try {
+    const user = await User.findOne({
+      where: {
+        id: req.params.id
+      }
+    });
+    res.json({ user });
+  } catch (e) {
+    console.log(e);
+    res.stats(500).send(e.message);
+  }
+});
+
+// Register route
+usersRouter.post('/', async (req, res) => {
+  try {
+    const { email, password, username, picture } = req.body;
+    if (password) {
+      const passwordDigest = await hash(password);
+      const user = await User.create({
+        email,
+        password_digest: passwordDigest,
+        name,
+      });
+      const userData = {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+        picture: user.picture,
+      };
+
+      const token = encode(userData);
+
+      res.json({
+        token,
+        userData,
+      });
+    }
+    else {
+      const user = await User.create({
+        email,
+        name,
+        picture
+      });
+
+      const userData = {
+        email: user.email,
+        name: user.name,
+        picture: user.picture,
+        created_at: user.created_at,
+        updated_at: user.updated_at,
+      };
+
+      const token = encode(userData);
+
+      res.json({
+        token,
+        userData,
+      });
+    }
+  } catch(e) {
+    console.error(e);
+  }
+});
+
+//Login route
+usersRouter.post('/login', async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({
+      where: {
+        email,
+      },
+    });
+    if (user !== null) {
+      const authenticated = await compare(password, user.password_digest);
+      if (authenticated === true) {
+        const userData = {
+          id: user.id,
+          name: user.name,
+          email: user.email,
+          picture: user.picture,
+        };
+
+        const token = encode(userData);
+
+        res.json({
+          token,
+          userData,
+        });
+      }
+      return res.status(401).send('Invalid Credentials');
+    }
+    return res.status(401).send('Invalid Credentials');
+  } catch(e) {
+    console.error(e);
+  }
+});
+
+module.exports = usersRouter;
+
--- server.js
+++ server.js
@@ -7,6 +7,8 @@ const usersRouter = require('./routes/usersRouter');
 const PORT = process.env.PORT || 3001;
 
 const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
 
 app.use(cors());
 app.use(logger('dev'));
@@ -17,6 +19,12 @@ app.use(‘/users', usersRouter);
 app.get('/', async(req, res) => {
   res.json({ msg: 'Welcome to class'})
 })
+
+io.on('connection', (socket) => {
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+  });
+});
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
