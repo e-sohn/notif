b5c8e25a48b0ece4f6aa5e3b24d59b9048422c7d
Eric <ericsohny@gmail.com>
2019-06-23 22:42:21 -0400

    Initialize Header, Main, and Footer components

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { withRouter } from 'react-router';
+
+function Footer() {
+  return (
+    <div className="Footer">
+      Footer 
+    </div>
+  );
+}
+
+export default withRouter(Footer);
---
+++ client/src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { withRouter } from 'react-router';
+
+function Header() {
+  return (
+    <div className="Header">
+      Header 
+    </div>
+  );
+}
+
+export default withRouter(Header);
